8bc5dbe523dec739958450d18caccd06fca19e3c
Anna Sobanska <ania.webdev@gmail.com>
2021-08-31 12:37:12 +0200

    Sum numbers with using for loop

---
+++ Recursion/Recursion.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int sum=0;
+	int m=2, n=4;
+
+	for (int i=m; i <= n; i++) {
+		sum +=i;
+	}
+
+	cout <<"Sum = " <<sum << endl;
+
+	system("pause>0");
+}
