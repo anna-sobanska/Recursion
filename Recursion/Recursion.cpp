c9fea741c9259723140009f9721e5d161ee7d92e
Anna Sobanska <ania.webdev@gmail.com>
2021-08-31 13:04:24 +0200

    Add recursivePower function

--- Recursion/Recursion.cpp
+++ Recursion/Recursion.cpp
@@ -7,12 +7,19 @@ int recursiveSum(int m, int n) {
 	return m + recursiveSum(m + 1, n);
 }
 
+int recursivePower(int basic, int index) {
+	if (index == 1)
+		return basic;
+	return basic * recursivePower(basic, index - 1);
+}
+
 int main() {
 
 	int sum=0;
 	int m=2, n=4;
 
 	cout <<"Sum = " <<recursiveSum(m,n) << endl;
+	cout <<"Power = " << recursivePower(m, n) << endl;
 
 	system("pause>0");
 }
