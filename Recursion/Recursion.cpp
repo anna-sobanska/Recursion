b56ec07573ea6fd80eba6667ba93c6957c9a4b4f
Anna Sobanska <ania.webdev@gmail.com>
2021-08-31 12:51:11 +0200

    Sum numbers using recursion

--- Recursion/Recursion.cpp
+++ Recursion/Recursion.cpp
@@ -1,16 +1,18 @@
 #include <iostream>
 using namespace std;
 
+int recursiveSum(int m, int n) {
+	if (m == n)
+		return m;
+	return m + recursiveSum(m + 1, n);
+}
+
 int main() {
 
 	int sum=0;
 	int m=2, n=4;
 
-	for (int i=m; i <= n; i++) {
-		sum +=i;
-	}
-
-	cout <<"Sum = " <<sum << endl;
+	cout <<"Sum = " <<recursiveSum(m,n) << endl;
 
 	system("pause>0");
 }
